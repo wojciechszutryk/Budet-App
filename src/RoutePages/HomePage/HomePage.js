b48df42c5d36cb908c5568e4602558c7c235778d
szutryk <wojtekszutryk@gmail.com>
2021-04-19 18:59:27 +0200

    add SadMac component, clean code

--- src/RoutePages/HomePage/HomePage.js
+++ src/RoutePages/HomePage/HomePage.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import SadMac from "components/SadMac";
 
 const HomePage = () => {
     return (
-        <>
-            aaaaaaaaaa
-        </>
+        <SadMac sad={false}/>
     );
 };
 
--- src/components/SuspenseErrorBoundary/ErrorPage.js
+++ src/components/SuspenseErrorBoundary/ErrorPage.js
@@ -1,32 +1,13 @@
 import React from 'react';
 import {StyledErrorPage} from "./StyledErrorPage";
 import {ResetButton} from "../Button/ButtonStyles";
+import SadMac from "../SadMac/SadMac";
 
 const ErrorPage = ({onClick}) => {
-    const balls = document.getElementsByClassName("ball");
-    document.onmousemove = function(event){
-        const x = event.clientX * 100 / window.innerWidth + "%";
-        const y = event.clientY * 100 / window.innerHeight + "%";
-
-        for (let i = 0; i < 2; i++) {
-            balls[i].style.left = x;
-            balls[i].style.top = y;
-            balls[i].style.transform = "translate(-" + x + ",-" + y + ")";
-        }
-    }
     return (
         <StyledErrorPage>
             <figure>
-                <div className="sad-mac">
-                    <div className="eyes">
-                        <div className="eye">
-                            <div className="ball"/>
-                        </div>
-                        <div className="eye">
-                            <div className="ball"/>
-                        </div>
-                    </div>
-                </div>
+                <SadMac/>
                 <figcaption>
                     <span className="errorMessage">{'error occurred'}</span>
                 </figcaption>
